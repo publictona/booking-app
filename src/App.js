e420bed7d5418044a684856e35bc2149a12bffa1
publictona <landgesushma2@gmail.com>
2022-10-14 10:12:56 +0530

    new project

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
-import { Box, Stack } from '@mui/system'
-import React ,{useState} from 'react'
-import Feed from './component/Feed'
-import Navbar from './component/Navbar'
-import Rightbar from './component/Rightbar'
-import Sidebar from './component/Sidebar'
-import Add from "./component/Add"
-import { createTheme, ThemeProvider } from '@mui/material'
+
+
+import Navbar from "./component/Navbar"
 
 const App = () => {
 
-  // const  [mode , setMode] = useState("dark")
-  // const darkTheme = createTheme({
-  //   palette :{
-  //     mode:mode,
-  //   }
-  // })
-
-
-
-
-
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <Box bgcolor={"background.default"} color={"text.primary"}>
-      <Navbar/>
-      <Stack direction="2" spacing={3} justifyContent="space-between" >
-      <Sidebar  />
-      <Feed></Feed>
-      <Rightbar/>
-      </Stack>
-      <Add/>
-    </Box>
-    </ThemeProvider>
+  
+return (
+  <div>
+    <Navbar/>
+  </div>
+   
+      
   )
 }
 
